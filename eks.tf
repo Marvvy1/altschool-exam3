ea2d2200d5ce5d6bfa726188e69545188f2e5246
Adeyinka Marvellous <ayomarvie@gmail.com>
2023-03-18 21:32:10 +0100

    my exam commit

---
+++ eks.tf
@@ -0,0 +1,40 @@
+provider "kubernetes" {
+    host = data.aws_eks_cluster.myapp-cluster.endpoint
+    token = data.aws_eks_cluster_auth.myapp-cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.myapp-cluster.certificate_authority.0.data)
+}
+
+data "aws_eks_cluster" "myapp-cluster" {
+    name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "myapp-cluster" {
+    name = module.eks.cluster_name 
+}
+
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.10.0"
+
+  cluster_name = "myapp-cluster"
+  cluster_version = "1.22"
+
+  subnet_ids = module.myapp-vpc.private_subnets
+  vpc_id = module.myapp-vpc.vpc_id
+
+  tags = {
+      environment = "development"
+      application = "myapp"
+  }
+
+  eks_managed_node_groups = {
+    dev = {
+        min_size = 1
+        max_size = 3
+        desired_size = 2
+
+        instnce_types = ["t2.medium"]
+    }
+  }
+}
